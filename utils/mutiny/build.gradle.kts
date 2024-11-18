b5f6e4086ca9756b394de0fd43ee08f510ffe24f
Debop <sunghyouk.bae@gmail.com>
2024-11-18 23:27:53 +0900

    add bluetape4k-mutiny module
    Signed-off-by: Debop <sunghyouk.bae@gmail.com>

---
+++ utils/mutiny/build.gradle.kts
@@ -0,0 +1,19 @@
+configurations {
+    testImplementation.get().extendsFrom(compileOnly.get(), runtimeOnly.get())
+}
+
+dependencies {
+    api(project(":bluetape4k-core"))
+    testImplementation(project(":bluetape4k-junit5"))
+    testImplementation(project(":bluetape4k-idgenerators"))
+
+    // Smallrye Mutiny
+    api(Libs.mutiny)
+    api(Libs.mutiny_kotlin)
+
+    // Coroutines
+    api(project(":bluetape4k-coroutines"))
+    api(Libs.kotlinx_coroutines_core)
+    compileOnly(Libs.kotlinx_coroutines_reactive)
+    testImplementation(Libs.kotlinx_coroutines_test)
+}
