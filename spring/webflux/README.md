9b291cb82efc22a37645b94c4ffa59c7b7898421
Debop <sunghyouk.bae@gmail.com>
2024-11-19 22:07:33 +0900

    add bluetape4k-spring-webflux module
    Signed-off-by: Debop <sunghyouk.bae@gmail.com>

---
+++ spring/webflux/README.md
@@ -0,0 +1,3 @@
+# Module bluetape4k-spring-webflux
+
+Spring Webflux 를 사용하는 프로젝트에서 사용할 수 있는 Kotlin 확장 기능을 제공합니다.
---
+++ spring/webflux/src/test/kotlin/io/bluetape4k/spring/webflux/AbstractWebfluxTest.kt
@@ -0,0 +1,9 @@
+package io.bluetape4k.spring.webflux
+
+import io.bluetape4k.logging.KLogging
+
+abstract class AbstractWebfluxTest {
+
+    companion object: KLogging()
+
+}
