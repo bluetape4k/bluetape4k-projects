87cd28335aa88588561ef90ecdac6611f544cdd8
Debop <sunghyouk.bae@gmail.com>
2024-11-20 20:40:25 +0900

    polishing
    Signed-off-by: Debop <sunghyouk.bae@gmail.com>

--- aws/core/src/main/kotlin/io/bluetape4k/aws/exceptions/AwsBluetapeException.kt
+++ aws/core/src/main/kotlin/io/bluetape4k/aws/exceptions/AwsBluetapeException.kt
@@ -5,7 +5,7 @@ import io.bluetape4k.exceptions.BluetapeException
 /**
  * AWS 관련 예외를 처리하기 위한 최상위 예외 클래스입니다.
  */
-open class AwsKommonsException: BluetapeException {
+open class AwsBluetapeException: BluetapeException {
     constructor(): super()
     constructor(message: String): super(message)
     constructor(message: String, cause: Throwable): super(message, cause)
