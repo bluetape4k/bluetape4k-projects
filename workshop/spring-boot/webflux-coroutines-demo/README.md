28d0a5b906f595425142c1a460cdc07421a08b5c
Debop <sunghyouk.bae@gmail.com>
2024-11-21 22:43:49 +0900

    add Spring Webflux + Coroutines demo project
    Signed-off-by: Debop <sunghyouk.bae@gmail.com>

---
+++ workshop/spring-boot/webflux-coroutines-demo/README.md
@@ -0,0 +1,3 @@
+# Webflux Coroutiones Demo
+
+Spring Boot Webflux 를 Kotlin Coroutines 로 사용하는 예제입니다.
---
+++ workshop/spring-boot/webflux-coroutines-demo/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    kotlin("plugin.spring")
+    id(Plugins.spring_boot)
+}
+
+springBoot {
+    mainClass.set("io.bluetape4k.workshop.coroutines.CoroutineApplicationKt")
+}
+
+configurations {
+    testImplementation.get().extendsFrom(compileOnly.get(), runtimeOnly.get())
+}
+
+dependencies {
+    api(project(":bluetape4k-spring-core"))
+    implementation(project(":bluetape4k-jackson"))
+    implementation(project(":bluetape4k-netty"))
+    testImplementation(project(":bluetape4k-junit5"))
+
+    // Spring Boot
+    implementation(Libs.springBoot("autoconfigure"))
+    annotationProcessor(Libs.springBoot("autoconfigure-processor"))
+    annotationProcessor(Libs.springBoot("configuration-processor"))
+    runtimeOnly(Libs.springBoot("devtools"))
+
+    implementation(Libs.springBootStarter("webflux"))
+    implementation(Libs.springBootStarter("mustache"))
+    implementation(Libs.springBootStarter("actuator"))
+    implementation(Libs.springBootStarter("validation"))
+
+    testImplementation(Libs.springBootStarter("test")) {
+        exclude(group = "junit", module = "junit")
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+        exclude(module = "mockito-core")
+    }
+
+    // Coroutines
+    implementation(project(":bluetape4k-coroutines"))
+    implementation(Libs.kotlinx_coroutines_core)
+    implementation(Libs.kotlinx_coroutines_reactor)
+    testImplementation(Libs.kotlinx_coroutines_test)
+
+    // Reactor
+    implementation(Libs.reactor_netty)
+    implementation(Libs.reactor_kotlin_extensions)
+    testImplementation(Libs.reactor_test)
+}
