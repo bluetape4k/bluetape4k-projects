a9b3347eafce7f247d4356cf91ffe8396dfee362
Debop <sunghyouk.bae@gmail.com>
2024-11-26 21:37:09 +0900

    WebTestExtensions 함수 수정
    Signed-off-by: Debop <sunghyouk.bae@gmail.com>

--- spring/tests/src/main/kotlin/io/bluetape4k/spring/tests/WebTestClientExtensions.kt
+++ spring/tests/src/main/kotlin/io/bluetape4k/spring/tests/WebTestClientExtensions.kt
@@ -7,37 +7,37 @@ fun WebTestClient.httpGet(uri: String, httpStatus: HttpStatus = HttpStatus.OK) =
     get()
         .uri(uri)
         .exchange()
-        .apply { expectStatus().isEqualTo(httpStatus) }
+        .expectStatus().isEqualTo(httpStatus)
 
 fun WebTestClient.httpHead(uri: String, httpStatus: HttpStatus = HttpStatus.OK) =
     head()
         .uri(uri)
         .exchange()
-        .apply { expectStatus().isEqualTo(httpStatus) }
+        .expectStatus().isEqualTo(httpStatus)
 
 fun WebTestClient.httpPost(uri: String, value: Any? = null, httpStatus: HttpStatus = HttpStatus.OK) =
     post()
         .uri(uri)
         .apply { value?.run { bodyValue(this) } }
         .exchange()
-        .apply { expectStatus().isEqualTo(httpStatus) }
+        .expectStatus().isEqualTo(httpStatus)
 
 fun WebTestClient.httpPut(uri: String, value: Any? = null, httpStatus: HttpStatus = HttpStatus.OK) =
     put()
         .uri(uri)
         .apply { value?.run { bodyValue(this) } }
         .exchange()
-        .apply { expectStatus().isEqualTo(httpStatus) }
+        .expectStatus().isEqualTo(httpStatus)
 
 fun WebTestClient.httpPatch(uri: String, value: Any? = null, httpStatus: HttpStatus = HttpStatus.OK) =
     patch()
         .uri(uri)
         .apply { value?.run { bodyValue(this) } }
         .exchange()
-        .apply { expectStatus().isEqualTo(httpStatus) }
+        .expectStatus().isEqualTo(httpStatus)
 
 fun WebTestClient.httpDelete(uri: String, httpStatus: HttpStatus = HttpStatus.OK) =
     delete()
         .uri(uri)
         .exchange()
-        .apply { expectStatus().isEqualTo(httpStatus) }
+        .expectStatus().isEqualTo(httpStatus) 
