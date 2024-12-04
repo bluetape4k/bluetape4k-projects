77d929da4ed4e901f1cc109a284d29fc823041cd
Debop <sunghyouk.bae@gmail.com>
2024-12-04 20:31:19 +0900

    chore: 테스트 코드 정리
    Signed-off-by: Debop <sunghyouk.bae@gmail.com>

--- testing/testcontainers/src/test/kotlin/io/bluetape4k/testcontainers/mq/RabbitMQServerTest.kt
+++ testing/testcontainers/src/test/kotlin/io/bluetape4k/testcontainers/mq/RabbitMQServerTest.kt
@@ -10,6 +10,7 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.amshove.kluent.shouldBeTrue
 import org.amshove.kluent.shouldNotBeNull
 import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.parallel.Execution
 import org.junit.jupiter.api.parallel.ExecutionMode
@@ -24,74 +25,80 @@ class RabbitMQServerTest {
         private const val RABBITMQ_TEST_MESSAGE = "Hello world"
     }
 
-    private val rabbitMQ = RabbitMQServer.Launcher.rabbitMQ
+    @Nested
+    inner class Docker {
 
-    @AfterAll
-    fun afterAll() {
-        rabbitMQ.close()
+        private val rabbitMQ = RabbitMQServer.Launcher.rabbitMQ
+
+        @AfterAll
+        fun afterAll() {
+            rabbitMQ.close()
+        }
+
+        @Test
+        fun `run rabbitmq server`() {
+            RabbitMQServer.Launcher.rabbitMQ.isRunning.shouldBeTrue()
+            log.debug { "host=${rabbitMQ.host}" }
+            log.debug { "port=${rabbitMQ.port}" }
+        }
+
+        @Test
+        fun `connect to rabbitmq server`() {
+            val factory = ConnectionFactory().apply {
+                host = rabbitMQ.host
+                port = rabbitMQ.port
+            }
+
+            val connection = factory.newConnection()
+            connection.shouldNotBeNull()
+
+            val channel = connection.createChannel()
+            channel.shouldNotBeNull()
+            channel.exchangeDeclare(RABBITMQ_TEST_EXCHANGE, "direct", true)
+
+            val queueName = channel.queueDeclare().queue
+            channel.queueBind(queueName, RABBITMQ_TEST_EXCHANGE, RABBITMQ_TEST_ROUTING_KEY)
+
+            // Set up a consumer on the queue
+            var messageWasReceived = false
+            channel.basicConsume(queueName, false, object: DefaultConsumer(channel) {
+                override fun handleDelivery(
+                    consumerTag: String?,
+                    envelope: Envelope?,
+                    properties: AMQP.BasicProperties?,
+                    body: ByteArray?,
+                ) {
+                    messageWasReceived = Arrays.equals(body, RABBITMQ_TEST_MESSAGE.toByteArray())
+                }
+            })
+
+            // post a message
+            channel.basicPublish(
+                RABBITMQ_TEST_EXCHANGE,
+                RABBITMQ_TEST_ROUTING_KEY,
+                null,
+                RABBITMQ_TEST_MESSAGE.toByteArray()
+            )
+
+            // check the message was received
+            Thread.sleep(1000)
+            messageWasReceived.shouldBeTrue()
+
+            connection.close()
+        }
     }
 
-    @Test
-    fun `run rabbitmq server`() {
-        RabbitMQServer.Launcher.rabbitMQ.isRunning.shouldBeTrue()
-        log.debug { "host=${rabbitMQ.host}" }
-        log.debug { "port=${rabbitMQ.port}" }
-    }
-
-    @Test
-    fun `run rabbitmq server with default port`() {
-        RabbitMQServer(useDefaultPort = true)
-            .apply { start() }
-            .use { rabbitmq ->
+    @Nested
+    inner class Default {
+        @Test
+        fun `run rabbitmq server with default port`() {
+            RabbitMQServer(useDefaultPort = true).use { rabbitmq ->
+                rabbitmq.start()
                 rabbitmq.isRunning.shouldBeTrue()
                 log.debug { "url = ${rabbitmq.url}" }
 
                 rabbitmq.port shouldBeEqualTo RabbitMQServer.AMQP_PORT
             }
-    }
-
-    @Test
-    fun `connect to rabbitmq server`() {
-        val factory = ConnectionFactory().apply {
-            host = rabbitMQ.host
-            port = rabbitMQ.port
         }
-
-        val connection = factory.newConnection()
-        connection.shouldNotBeNull()
-
-        val channel = connection.createChannel()
-        channel.shouldNotBeNull()
-        channel.exchangeDeclare(RABBITMQ_TEST_EXCHANGE, "direct", true)
-
-        val queueName = channel.queueDeclare().queue
-        channel.queueBind(queueName, RABBITMQ_TEST_EXCHANGE, RABBITMQ_TEST_ROUTING_KEY)
-
-        // Set up a consumer on the queue
-        var messageWasReceived = false
-        channel.basicConsume(queueName, false, object: DefaultConsumer(channel) {
-            override fun handleDelivery(
-                consumerTag: String?,
-                envelope: Envelope?,
-                properties: AMQP.BasicProperties?,
-                body: ByteArray?,
-            ) {
-                messageWasReceived = Arrays.equals(body, RABBITMQ_TEST_MESSAGE.toByteArray())
-            }
-        })
-
-        // post a message
-        channel.basicPublish(
-            RABBITMQ_TEST_EXCHANGE,
-            RABBITMQ_TEST_ROUTING_KEY,
-            null,
-            RABBITMQ_TEST_MESSAGE.toByteArray()
-        )
-
-        // check the message was received
-        Thread.sleep(1000)
-        messageWasReceived.shouldBeTrue()
-
-        connection.close()
     }
 }
