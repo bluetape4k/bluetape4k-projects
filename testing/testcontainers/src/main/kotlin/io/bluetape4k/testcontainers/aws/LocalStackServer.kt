c0c37ba0ea08d868ffac591474837cc0aef35f94
Debop <sunghyouk.bae@gmail.com>
2024-12-05 11:26:23 +0900

    chore: Bump up localstack docker image (4)
    Signed-off-by: Debop <sunghyouk.bae@gmail.com>

--- testing/testcontainers/src/main/kotlin/io/bluetape4k/testcontainers/aws/LocalStackServer.kt
+++ testing/testcontainers/src/main/kotlin/io/bluetape4k/testcontainers/aws/LocalStackServer.kt
@@ -39,7 +39,7 @@ class LocalStackServer private constructor(
     companion object: KLogging() {
         const val IMAGE = "localstack/localstack"
         const val NAME = "localstack"
-        const val TAG = "3.6"
+        const val TAG = "4"
         const val PORT = 4566
 
         /**
