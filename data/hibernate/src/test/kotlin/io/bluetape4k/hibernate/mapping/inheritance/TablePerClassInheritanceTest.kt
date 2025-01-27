770aaa87bc3b9f5ac283b508656f151ad5812145
Debop <sunghyouk.bae@gmail.com>
2025-01-27 21:08:57 +0900

    chore: SQL 정보 추가
    Signed-off-by: Debop <sunghyouk.bae@gmail.com>

--- data/hibernate/src/test/kotlin/io/bluetape4k/hibernate/mapping/inheritance/TablePerClassInheritanceTest.kt
+++ data/hibernate/src/test/kotlin/io/bluetape4k/hibernate/mapping/inheritance/TablePerClassInheritanceTest.kt
@@ -3,6 +3,7 @@ package io.bluetape4k.hibernate.mapping.inheritance
 import io.bluetape4k.ToStringBuilder
 import io.bluetape4k.hibernate.AbstractHibernateTest
 import io.bluetape4k.hibernate.model.AbstractJpaEntity
+import io.bluetape4k.support.uninitialized
 import jakarta.persistence.Access
 import jakarta.persistence.AccessType
 import jakarta.persistence.Entity
@@ -14,24 +15,69 @@ import jakarta.persistence.InheritanceType
 import jakarta.persistence.Table
 import jakarta.persistence.Temporal
 import jakarta.persistence.TemporalType
+import org.amshove.kluent.shouldBeEmpty
+import org.amshove.kluent.shouldBeEqualTo
+import org.amshove.kluent.shouldNotBeEmpty
 import org.hibernate.annotations.DynamicInsert
 import org.hibernate.annotations.DynamicUpdate
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.findByIdOrNull
 import java.util.*
 
 class TablePerClassInheritanceTest: AbstractHibernateTest() {
+
+    @Autowired
+    private val cardRepo: TablePerClassCreditCardRepository = uninitialized()
+
+    @Autowired
+    private val accountRepo: TablePerClassBankAccountRepository = uninitialized()
+
+    @Test
+    fun `entity has its own table`() {
+
+        val account = UuidBankAccount("debop").apply {
+            account = "123-accunt"
+            bankname = "KB"
+            swift = "kb-swift"
+        }
+        accountRepo.save(account)
+
+        val card = UuidCreditCard("debop").apply {
+            number = "1111-2222-3333-4444"
+            companyName = "KakaoBank"
+            expYear = 2024
+            expMonth = 12
+            startDate = Date()
+        }
+
+        cardRepo.save(card)
+
+        flushAndClear()
+
+        val account2 = accountRepo.findByIdOrNull(account.id)!!
+        account2 shouldBeEqualTo account
+
+        val card2 = cardRepo.findByIdOrNull(card.id)!!
+        card2 shouldBeEqualTo card
+
+        accountRepo.deleteAll()
+        accountRepo.flush()
+
+        // 다른 테이블이므로, bank account 정보가 삭제되어도 credit card 정보는 남아 있어야 한다.
+        cardRepo.findAll().shouldNotBeEmpty()
+
+        cardRepo.deleteAll()
+        flushAndClear()
+        cardRepo.findAll().shouldBeEmpty()
+    }
 }
 
 
 @Access(AccessType.FIELD)
 @Entity(name = "tableperclass_billing")
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-@Table(
-    name = "tableperclass_billing",
-    indexes = [
-        Index(name = "ix_tableperclass_billing_owner", columnList = "owner")
-    ]
-)
 abstract class AbstractUuidBilling(
     @Id
     @GeneratedValue(generator = "uuid")
@@ -49,7 +95,29 @@ abstract class AbstractUuidBilling(
     }
 }
 
+/**
+ * ```sql
+ * create table tableperclass_creditcard (
+ *         id uuid not null,
+ *         owner varchar(255),
+ *         company_name varchar(255),
+ *         end_date timestamp(6),
+ *         exp_month integer,
+ *         exp_year integer,
+ *         number varchar(255),
+ *         start_date timestamp(6),
+ *         swift varchar(255),
+ *         primary key (id)
+ *     )
+ * ```
+ */
 @Entity(name = "tableperclass_creditcard")
+@Table(
+    name = "tableperclass_creditcard",
+    indexes = [
+        Index(name = "ix_tableperclass_creditcard_owner", columnList = "owner")
+    ]
+)
 @Access(AccessType.FIELD)
 @DynamicInsert
 @DynamicUpdate
@@ -81,7 +149,25 @@ class UuidCreditCard(owner: String): AbstractUuidBilling(owner = owner) {
     }
 }
 
+/**
+ * ```sql
+ * create table tableperclass_bankaccount (
+ *         id uuid not null,
+ *         owner varchar(255),
+ *         account varchar(255),
+ *         bankname varchar(255),
+ *         swift varchar(255),
+ *         primary key (id)
+ *     )
+ * ```
+ */
 @Entity(name = "tableperclass_bankaccount")
+@Table(
+    name = "tableperclass_bankaccount",
+    indexes = [
+        Index(name = "ix_tableperclass_bankaccount_owner", columnList = "owner")
+    ]
+)
 @Access(AccessType.FIELD)
 @DynamicInsert
 @DynamicUpdate
