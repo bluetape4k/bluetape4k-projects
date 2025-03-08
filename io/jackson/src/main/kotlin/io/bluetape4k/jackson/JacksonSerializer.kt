778605d9c5555deffcb2ab421f64f1d33ff23cd8
Debop <sunghyouk.bae@gmail.com>
2025-03-09 01:41:54 +0900

    feat: Jackson column 구현 및 테스트 완료

--- io/jackson/src/main/kotlin/io/bluetape4k/jackson/JacksonSerializer.kt
+++ io/jackson/src/main/kotlin/io/bluetape4k/jackson/JacksonSerializer.kt
@@ -1,6 +1,7 @@
 package io.bluetape4k.jackson
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.json.JsonMapper
 import io.bluetape4k.logging.KLogging
 import io.bluetape4k.support.emptyByteArray
 
@@ -23,7 +24,7 @@ import io.bluetape4k.support.emptyByteArray
  * @param mapper Jackson [ObjectMapper] 인스턴스
  */
 open class JacksonSerializer(
-    val mapper: ObjectMapper = Jackson.defaultJsonMapper,
+    val mapper: JsonMapper = Jackson.defaultJsonMapper,
 ): JsonSerializer {
 
     companion object: KLogging()
@@ -36,3 +37,38 @@ open class JacksonSerializer(
         return bytes?.run { mapper.readValue(this, clazz) }
     }
 }
+
+
+/**
+ * [JsonSerializer]를 이용하여 [bytes]를 역직렬화하여 객체를 빌드합니다. 실패시 null 반환
+ *
+ * ```
+ * val serializer = JacksonSerializer()
+ * val bytes = serializer.serialize(data)
+ * val data = serializer.deserialize<Data>(bytes)
+ * ```
+ *
+ * @param T 역직렬화할 대상 수형
+ * @param bytes JSON 직렬화된 ByteArray
+ * @return 역직렬화된 객체, 실패 시 null 반환
+ */
+inline fun <reified T: Any> JacksonSerializer.deserialize(bytes: ByteArray?): T? =
+    bytes?.run { mapper.readValueOrNull<T>(bytes) }
+// deserialize(bytes, T::class.java)
+
+/**
+ * [JsonSerializer]를 이용하여 [jsonText]를 역직렬화하여 객체를 빌드합니다. 실패시 null 반환
+ *
+ * ```
+ * val serializer = JacksonSerializer()
+ * val jsonText = serializer.serializeAsString(data)
+ * val data = serializer.deserializeFromString<Data>(jsonText)
+ * ```
+ *
+ * @param T 역직렬화할 대상 수형
+ * @param jsonText JSON 직렬화된 문자열
+ * @return 역직렬화된 객체, 실패 시 null 반환
+ */
+inline fun <reified T: Any> JacksonSerializer.deserializeFromString(jsonText: String?): T? =
+    jsonText?.run { mapper.readValueOrNull<T>(jsonText) }
+// deserializeFromString(jsonText, T::class.java)  --> 이 것 쓰면 제대로 된 Kotlin 수형을 가져오지 못함 
