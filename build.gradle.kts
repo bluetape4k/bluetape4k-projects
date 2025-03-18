4440bbe4a149f83d9160f6ace79e783281488038
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-03-18 22:44:48 +0900

    build: github 배포 관련 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,13 +31,13 @@ plugins {
 
 // NOTE: Nexus 에 등록된 것 때문에 사용한다
 // NOTE: .zshrc 에 정의하던가, ~/.gradle/gradle.properties 에 정의해주셔야 합니다.
-//fun getEnvOrProjectProperty(propertyKey: String, envKey: String): String {
-//    return project.findProperty(propertyKey) as? String ?: System.getenv(envKey)
-//}
-//
-//val gprUser: String = getEnvOrProjectProperty("gpr.user", "GITHUB_USERNAME")
-//val gprKey: String = getEnvOrProjectProperty("gpr.key", "GITHUB_TOKEN")
-//val gprPublishKey: String = getEnvOrProjectProperty("gpr.publish.key", "GITHUB_PUBLISH_TOKEN")
+fun getEnvOrProjectProperty(propertyKey: String, envKey: String): String {
+    return project.findProperty(propertyKey) as? String ?: System.getenv(envKey)
+}
+
+val gprUser: String = getEnvOrProjectProperty("gpr.user", "GITHUB_USERNAME")
+val gprKey: String = getEnvOrProjectProperty("gpr.key", "GITHUB_TOKEN")
+val gprPublishKey: String = getEnvOrProjectProperty("gpr.publish.key", "GITHUB_PUBLISH_TOKEN")
 
 
 val projectGroup: String by project
@@ -51,14 +51,14 @@ allprojects {
     repositories {
         mavenCentral()
         google()
-//        maven {
-//            name = "bluetape4k"
-//            url = uri("https://maven.pkg.github.com/bluetape4k/bluetape4k-projects")
-//            credentials {
-//                username = gprUser
-//                password = gprKey
-//            }
-//        }
+        maven {
+            name = "bluetape4k"
+            url = uri("https://maven.pkg.github.com/bluetape4k/bluetape4k-projects")
+            credentials {
+                username = gprUser
+                password = gprKey
+            }
+        }
     }
 }
 
@@ -619,13 +619,14 @@ subprojects {
             }
         }
         repositories {
-//            maven {
-//                url = uri("https://maven.pkg.github.com/bluetape4k/bluetape4k-projects")
-//                credentials {
-//                    username = gprUser
-//                    password = gprPublishKey
-//                }
-//            }
+            maven {
+                name = "GitHub"
+                url = uri("https://maven.pkg.github.com/bluetape4k/bluetape4k-projects")
+                credentials {
+                    username = gprUser
+                    password = gprPublishKey
+                }
+            }
             mavenLocal()
         }
     }
