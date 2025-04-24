82264210cbe35babd1af4caef3121cc1e3e19c11
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-04-25 05:42:12 +0900

    fix: Redisson 기본 코덱을 Kryo5 -> Fury 로 변경

--- infra/redis/src/main/kotlin/io/bluetape4k/redis/redisson/RedissonCodecs.kt
+++ infra/redis/src/main/kotlin/io/bluetape4k/redis/redisson/RedissonCodecs.kt
@@ -6,7 +6,6 @@ import io.bluetape4k.redis.redisson.codec.GzipCodec
 import io.bluetape4k.redis.redisson.codec.Lz4Codec
 import io.bluetape4k.redis.redisson.codec.ProtobufCodec
 import io.bluetape4k.redis.redisson.codec.ZstdCodec
-import io.bluetape4k.support.unsafeLazy
 import org.redisson.client.codec.Codec
 import org.redisson.client.codec.DoubleCodec
 import org.redisson.client.codec.IntegerCodec
@@ -27,68 +26,69 @@ object RedissonCodecs: KLogging() {
      * Redisson 의 기본 Codec (Kryo5) 입니다.
      */
     @JvmStatic
-    val Default: Codec by unsafeLazy { Kryo5 }
+    val Default: Codec by lazy { Fury }
 
-    val Int: Codec by unsafeLazy { IntegerCodec() }
-    val Long: Codec by unsafeLazy { LongCodec() }
-    val Double: Codec by unsafeLazy { DoubleCodec() }
-    val String: Codec by unsafeLazy { StringCodec() }
+    val Int: Codec by lazy { IntegerCodec() }
+    val Long: Codec by lazy { LongCodec() }
+    val Double: Codec by lazy { DoubleCodec() }
+    val String: Codec by lazy { StringCodec() }
 
-    val Kryo5: Codec by unsafeLazy { Kryo5Codec() }
-    val Protobuf: Codec by unsafeLazy { ProtobufCodec() }
+    val Kryo5: Codec by lazy { Kryo5Codec() }
+    val Protobuf: Codec by lazy { ProtobufCodec() }
 
     /**
      * Apache Fury Serialization Codec
      */
-    val Fury: Codec by unsafeLazy { FuryCodec() }
+    val Fury: Codec by lazy { FuryCodec() }
 
     /**
      * JDK Serialization Codec
      */
-    val Jdk: Codec by unsafeLazy { SerializationCodec() }
+    val Jdk: Codec by lazy { SerializationCodec() }
 
-    val Kryo5Composite: Codec by unsafeLazy { CompositeCodec(String, Kryo5, Kryo5) }
-    val ProtobufComposite: Codec by unsafeLazy { CompositeCodec(String, Protobuf, Protobuf) }
-    val FuryComposite: Codec by unsafeLazy { CompositeCodec(String, Fury, Fury) }
-    val JdkComposite: Codec by unsafeLazy { CompositeCodec(String, Jdk, Jdk) }
+    val Kryo5Composite: Codec by lazy { CompositeCodec(String, Kryo5, Kryo5) }
+    val ProtobufComposite: Codec by lazy { CompositeCodec(String, Protobuf, Protobuf) }
+    val FuryComposite: Codec by lazy { CompositeCodec(String, Fury, Fury) }
+    val JdkComposite: Codec by lazy { CompositeCodec(String, Jdk, Jdk) }
 
-    val SnappyKryo5: Codec by unsafeLazy { SnappyCodecV2(Kryo5) }
-    val SnappyProtobuf: Codec by unsafeLazy { SnappyCodecV2(Protobuf) }
-    val SnappyFury: Codec by unsafeLazy { SnappyCodecV2(Fury) }
-    val SnappyJdk: Codec by unsafeLazy { SnappyCodecV2(Jdk) }
+    val GzipKryo5: Codec by lazy { GzipCodec(Kryo5) }
+    val GzipProtobuf: Codec by lazy { GzipCodec(Protobuf) }
+    val GzipFury: Codec by lazy { GzipCodec(Fury) }
+    val GzipJdk: Codec by lazy { GzipCodec(Jdk) }
 
-    val SnappyKryo5Composite: Codec by unsafeLazy { CompositeCodec(String, SnappyKryo5, SnappyKryo5) }
-    val SnappyProtobufComposite: Codec by unsafeLazy { CompositeCodec(String, SnappyProtobuf, SnappyProtobuf) }
-    val SnappyFuryComposite: Codec by unsafeLazy { CompositeCodec(String, SnappyFury, SnappyFury) }
-    val SnappyJdkComposite: Codec by unsafeLazy { CompositeCodec(String, SnappyJdk, SnappyJdk) }
+    val GzipKryo5Composite: Codec by lazy { CompositeCodec(String, GzipKryo5, GzipKryo5) }
+    val GzipProtobufComposite: Codec by lazy { CompositeCodec(String, GzipProtobuf, GzipProtobuf) }
+    val GzipFuryComposite: Codec by lazy { CompositeCodec(String, GzipFury, GzipFury) }
+    val GzipJdkComposite: Codec by lazy { CompositeCodec(String, GzipJdk, GzipJdk) }
 
-    val LZ4Kryo5: Codec by unsafeLazy { Lz4Codec(Kryo5) }
-    val LZ4Protobuf: Codec by unsafeLazy { Lz4Codec(Protobuf) }
-    val LZ4Fury: Codec by unsafeLazy { Lz4Codec(Fury) }
-    val LZ4Jdk: Codec by unsafeLazy { Lz4Codec(Jdk) }
+    val LZ4Kryo5: Codec by lazy { Lz4Codec(Kryo5) }
+    val LZ4Protobuf: Codec by lazy { Lz4Codec(Protobuf) }
+    val LZ4Fury: Codec by lazy { Lz4Codec(Fury) }
+    val LZ4Jdk: Codec by lazy { Lz4Codec(Jdk) }
 
-    val LZ4Kryo5Composite: Codec by unsafeLazy { CompositeCodec(String, LZ4Kryo5, LZ4Kryo5) }
-    val LZ4ProtobufComposite: Codec by unsafeLazy { CompositeCodec(String, LZ4Protobuf, LZ4Protobuf) }
-    val LZ4FuryComposite: Codec by unsafeLazy { CompositeCodec(String, LZ4Fury, LZ4Fury) }
-    val LZ4JdkComposite: Codec by unsafeLazy { CompositeCodec(String, LZ4Jdk, LZ4Jdk) }
+    val LZ4Kryo5Composite: Codec by lazy { CompositeCodec(String, LZ4Kryo5, LZ4Kryo5) }
+    val LZ4ProtobufComposite: Codec by lazy { CompositeCodec(String, LZ4Protobuf, LZ4Protobuf) }
+    val LZ4FuryComposite: Codec by lazy { CompositeCodec(String, LZ4Fury, LZ4Fury) }
+    val LZ4JdkComposite: Codec by lazy { CompositeCodec(String, LZ4Jdk, LZ4Jdk) }
 
-    val ZstdKryo5: Codec by unsafeLazy { ZstdCodec(Kryo5) }
-    val ZstdProtobuf: Codec by unsafeLazy { ZstdCodec(Protobuf) }
-    val ZstdFury: Codec by unsafeLazy { ZstdCodec(Fury) }
-    val ZstdJdk: Codec by unsafeLazy { ZstdCodec(Jdk) }
+    val SnappyKryo5: Codec by lazy { SnappyCodecV2(Kryo5) }
+    val SnappyProtobuf: Codec by lazy { SnappyCodecV2(Protobuf) }
+    val SnappyFury: Codec by lazy { SnappyCodecV2(Fury) }
+    val SnappyJdk: Codec by lazy { SnappyCodecV2(Jdk) }
 
-    val ZstdKryo5Composite: Codec by unsafeLazy { CompositeCodec(String, ZstdKryo5, ZstdKryo5) }
-    val ZstdProtobufComposite: Codec by unsafeLazy { CompositeCodec(String, ZstdProtobuf, ZstdProtobuf) }
-    val ZstdFuryComposite: Codec by unsafeLazy { CompositeCodec(String, ZstdFury, ZstdFury) }
-    val ZstdJdkComposite: Codec by unsafeLazy { CompositeCodec(String, ZstdJdk, ZstdJdk) }
+    val SnappyKryo5Composite: Codec by lazy { CompositeCodec(String, SnappyKryo5, SnappyKryo5) }
+    val SnappyProtobufComposite: Codec by lazy { CompositeCodec(String, SnappyProtobuf, SnappyProtobuf) }
+    val SnappyFuryComposite: Codec by lazy { CompositeCodec(String, SnappyFury, SnappyFury) }
+    val SnappyJdkComposite: Codec by lazy { CompositeCodec(String, SnappyJdk, SnappyJdk) }
 
-    val GzipKryo5: Codec by unsafeLazy { GzipCodec(Kryo5) }
-    val GzipProtobuf: Codec by unsafeLazy { GzipCodec(Protobuf) }
-    val GzipFury: Codec by unsafeLazy { GzipCodec(Fury) }
-    val GzipJdk: Codec by unsafeLazy { GzipCodec(Jdk) }
+    val ZstdKryo5: Codec by lazy { ZstdCodec(Kryo5) }
+    val ZstdProtobuf: Codec by lazy { ZstdCodec(Protobuf) }
+    val ZstdFury: Codec by lazy { ZstdCodec(Fury) }
+    val ZstdJdk: Codec by lazy { ZstdCodec(Jdk) }
 
-    val GzipKryo5Composite: Codec by unsafeLazy { CompositeCodec(String, GzipKryo5, GzipKryo5) }
-    val GzipProtobufComposite: Codec by unsafeLazy { CompositeCodec(String, GzipProtobuf, GzipProtobuf) }
-    val GzipFuryComposite: Codec by unsafeLazy { CompositeCodec(String, GzipFury, GzipFury) }
-    val GzipJdkComposite: Codec by unsafeLazy { CompositeCodec(String, GzipJdk, GzipJdk) }
+    val ZstdKryo5Composite: Codec by lazy { CompositeCodec(String, ZstdKryo5, ZstdKryo5) }
+    val ZstdProtobufComposite: Codec by lazy { CompositeCodec(String, ZstdProtobuf, ZstdProtobuf) }
+    val ZstdFuryComposite: Codec by lazy { CompositeCodec(String, ZstdFury, ZstdFury) }
+    val ZstdJdkComposite: Codec by lazy { CompositeCodec(String, ZstdJdk, ZstdJdk) }
+
 }
