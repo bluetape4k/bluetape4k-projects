600ebc6676a29c1a41f57170d68b44a6173c395e
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-04-27 14:17:01 +0900

    feat: Bluetape4k Exposed Redisson 모듈 추가 : Redisson과 Exposed를 사용한 다양한 캐시 전략 구현

---
+++ data/exposed-redisson/README.md
@@ -0,0 +1,5 @@
+# Module Bluetape4k Exposed Redisson
+
+다양한 캐시 전략을 Redisson, Exposed 로 구현합니다.
+
+지원하는 캐시 전략은 Read Through, Write Through, Write Behind 전략을 지원하고, 동기 (Synchronous), 코루틴을 사용한 비동기 방식(Asynchronous) 으로 지원합니다.
---
+++ data/exposed-redisson/build.gradle.kts
@@ -0,0 +1,60 @@
+@Suppress("UnstableApiUsage")
+configurations {
+    testImplementation.get().extendsFrom(compileOnly.get(), runtimeOnly.get())
+}
+
+dependencies {
+    // Exposed
+    implementation(platform(Libs.exposed_bom))
+
+    api(project(":bluetape4k-exposed"))
+    api(Libs.exposed_core)
+    api(Libs.exposed_jdbc)
+    api(Libs.exposed_dao)
+    testImplementation(Libs.exposed_java_time)
+    testImplementation(Libs.exposed_spring_boot_starter)
+    testImplementation(project(":bluetape4k-exposed-tests"))
+
+    // Redisson
+    api(project(":bluetape4k-redis"))
+    api(Libs.redisson)
+
+    // Codecs
+    implementation(project(":bluetape4k-io"))
+    implementation(Libs.kryo)
+    implementation(Libs.fury_kotlin)
+
+    // Compressor
+    implementation(Libs.snappy_java)
+    implementation(Libs.lz4_java)
+    implementation(Libs.zstd_jni)
+
+    // Coroutines
+    implementation(project(":bluetape4k-coroutines"))
+    implementation(Libs.kotlinx_coroutines_core)
+    testImplementation(Libs.kotlinx_coroutines_test)
+
+    // Bluetape4k Modules for Testing
+    testImplementation(project(":bluetape4k-junit5"))
+    testImplementation(project(":bluetape4k-testcontainers"))
+    testImplementation(Libs.testcontainers_junit_jupiter)
+    testImplementation(Libs.testcontainers_mariadb)
+    testImplementation(Libs.testcontainers_mysql)
+    testImplementation(Libs.testcontainers_postgresql)
+
+    testImplementation(project(":bluetape4k-idgenerators"))
+    testImplementation(project(":bluetape4k-javatimes"))
+
+    // Database Drivers
+    testImplementation(Libs.hikaricp)
+    testImplementation(Libs.h2_v2)
+    testImplementation(Libs.mariadb_java_client)
+    testImplementation(Libs.mysql_connector_j)
+    testImplementation(Libs.postgresql_driver)
+    testImplementation(Libs.pgjdbc_ng)
+
+    testImplementation(Libs.springBootStarter("test")) {
+        exclude(group = "junit", module = "junit")
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+}
