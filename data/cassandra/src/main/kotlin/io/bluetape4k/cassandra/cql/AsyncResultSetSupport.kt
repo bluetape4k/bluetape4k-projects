b528de4c7ee68102f643092991fc724ee258981f
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-05-16 11:49:26 +0900

    refactor: AsyncResultSet 및 Row 처리 로직 개선 - Flow 생성 로직에서 channelFlow로 변경 - 코드 가독성을 위해 if-else 블록 포맷 수정 - KLogging을 KLoggingChannel로 변경 - 로그 레벨 DEBUG에서 INFO로 업데이트

--- data/cassandra/src/main/kotlin/io/bluetape4k/cassandra/cql/AsyncResultSetSupport.kt
+++ data/cassandra/src/main/kotlin/io/bluetape4k/cassandra/cql/AsyncResultSetSupport.kt
@@ -3,10 +3,7 @@ package io.bluetape4k.cassandra.cql
 import com.datastax.oss.driver.api.core.cql.AsyncResultSet
 import com.datastax.oss.driver.api.core.cql.Row
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.future.await
 
 /**
@@ -20,17 +17,21 @@ import kotlinx.coroutines.future.await
  *    // ...
  * }
  */
-fun AsyncResultSet.asFlow(): Flow<Row> = flow {
-    var current = this@asFlow
-    while (current.remaining() > 0) {
-        emitAll(current.currentPage().asFlow())
-        if (current.hasMorePages()) {
-            current = current.fetchNextPage().await()
-        } else {
-            break
-        }
-    }
-}
+fun AsyncResultSet.asFlow(): Flow<Row> = asFlow { it }
+//    channelFlow {
+//        var current = this@asFlow
+//        while (current.remaining() > 0) {
+//            // emitAll(current.currentPage().asFlow())
+//            current.currentPage().forEach { row ->
+//                send(row)
+//            }
+//            if (current.hasMorePages()) {
+//                current = current.fetchNextPage().await()
+//            } else {
+//                break
+//            }
+//        }
+//    }
 
 /**
  * [AsyncResultSet]의 현재 Page 뿐 아니라 Next Page들을 계속 읽어서 [mapper]를 이용한 변환한 값을 emit 하는 [Flow]로 반환합니다.
@@ -43,10 +44,12 @@ fun AsyncResultSet.asFlow(): Flow<Row> = flow {
  * @param mapper [Row]를 변환할 함수
  * @return [Flow] 인스턴스
  */
-fun <T> AsyncResultSet.asFlow(mapper: suspend (row: Row) -> T): Flow<T> = flow {
+inline fun <T> AsyncResultSet.asFlow(crossinline mapper: suspend (row: Row) -> T): Flow<T> = channelFlow {
     var current = this@asFlow
     while (current.remaining() > 0) {
-        emitAll(current.currentPage().asFlow().map(mapper))
+        current.currentPage().forEach { row ->
+            send(mapper(row))
+        }
         if (current.hasMorePages()) {
             current = current.fetchNextPage().await()
         } else {
