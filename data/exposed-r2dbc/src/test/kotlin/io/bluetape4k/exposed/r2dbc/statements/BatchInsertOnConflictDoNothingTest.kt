501596b9c4d751079031ca128596c4b6c19141bd
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-05-27 17:40:52 +0900

    feat: Exposed-R2DBC 확장 추가 및 테스트 구현 - 새로운 TableExtensions, QueryExtensions, ReadableExtensions 추가 - virtualThreadTransaction 처리 추가 - ImplicitSelectAll 기능 및 배치 삽입(on conflict do nothing) 지원 추가 - BoardSchema 정의 및 테스트 데이터 준비 - R2dbcConfigurationTest 개선 (코루틴 사용 수정) - 관련 테스트 케이스 작성 및 테스트 데이터 로드 이미지 추가 - dependency 변화:   - api r2dbc-pool 추가   - R2dbc 내에서 postgresql dependency 공식적으로 compileOnly 로 수정

---
+++ data/exposed-r2dbc/src/test/kotlin/io/bluetape4k/exposed/r2dbc/statements/BatchInsertOnConflictDoNothingTest.kt
@@ -0,0 +1,60 @@
+package io.bluetape4k.exposed.r2dbc.statements
+
+import io.bluetape4k.exposed.r2dbc.tests.R2dbcExposedTestBase
+import io.bluetape4k.exposed.r2dbc.tests.TestDB
+import io.bluetape4k.exposed.r2dbc.tests.withTables
+import io.bluetape4k.junit5.coroutines.runSuspendIO
+import io.bluetape4k.logging.coroutines.KLoggingChannel
+import org.amshove.kluent.shouldBeEqualTo
+import org.jetbrains.exposed.v1.core.Table
+import org.jetbrains.exposed.v1.r2dbc.batchInsert
+import org.jetbrains.exposed.v1.r2dbc.insert
+import org.junit.jupiter.api.Assumptions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+class BatchInsertOnConflictDoNothingTest: R2dbcExposedTestBase() {
+
+    companion object: KLoggingChannel()
+
+    /**
+     * Batch Insert with ON CONFLICT DO NOTHING - [batchInsert] 시, 예외가 발생하면 해당 예외를 무시하고, 다음 작업을 수행합니다.
+     *
+     * ```sql
+     * -- Postgres
+     * CREATE TABLE IF NOT EXISTS tester (id VARCHAR(10) NOT NULL);
+     * ALTER TABLE tester ADD CONSTRAINT tester_id_unique UNIQUE (id);
+     *
+     * INSERT INTO tester (id) VALUES ('foo');
+     * INSERT INTO tester (id) VALUES ('foo') ON CONFLICT (id) DO NOTHING;
+     * INSERT INTO tester (id) VALUES ('bar') ON CONFLICT (id) DO NOTHING;
+     * ```
+     */
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun `batch insert number of inserted rows`(testDB: TestDB) = runSuspendIO {
+        Assumptions.assumeTrue { testDB in (TestDB.ALL_MYSQL + TestDB.ALL_POSTGRES_LIKE) }
+
+        val tester = object: Table("tester") {
+            val id = varchar("id", 10).uniqueIndex()
+        }
+
+        withTables(testDB, tester) {
+            tester.insert { it[id] = "foo" }
+
+            val statement = BatchInsertOnConflictDoNothing(tester)
+            val executable = BatchInsertOnConflictDoNothingExecutable(statement)
+
+            val numInserted = executable.run {
+                statement.addBatch()
+                statement[tester.id] = "foo"        // 중복되므로 insert 되지 않음
+
+                statement.addBatch()
+                statement[tester.id] = "bar"        // 중복되지 않으므로 추가됨
+
+                execute(this@withTables)
+            }
+            numInserted shouldBeEqualTo 1
+        }
+    }
+}
