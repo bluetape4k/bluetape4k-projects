77392ca9387f04caf0e188b856f24748fa91b26f
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-07-11 04:37:44 +0900

    feat: Add windowed, chunked, and sliding functions for IntInterval and LongInterval

--- bluetape4k/collections/src/main/kotlin/io/bluetape4k/collections/eclipse/ranges/IntervalExtensions.kt
+++ bluetape4k/collections/src/main/kotlin/io/bluetape4k/collections/eclipse/ranges/IntervalExtensions.kt
@@ -2,8 +2,7 @@ package io.bluetape4k.collections.eclipse.ranges
 
 import io.bluetape4k.collections.eclipse.primitives.intArrayList
 import io.bluetape4k.collections.eclipse.primitives.longArrayList
-import io.bluetape4k.collections.eclipse.primitives.toIntArrayList
-import io.bluetape4k.collections.eclipse.primitives.toLongArrayList
+import io.bluetape4k.support.requireGt
 import org.eclipse.collections.impl.list.mutable.primitive.IntArrayList
 import org.eclipse.collections.impl.list.mutable.primitive.LongArrayList
 import org.eclipse.collections.impl.list.primitive.IntInterval
@@ -19,16 +18,47 @@ inline fun IntInterval.forEach(crossinline block: (Int) -> Unit) {
     this.each { block(it) }
 }
 
-fun IntInterval.chunked(chunkSize: Int): Sequence<IntArrayList> = sequence {
-    this@chunked
-        .chunk(chunkSize)
-        .forEach { chunked ->
-            yield(chunked.toIntArrayList())
+inline fun IntInterval.windowed(
+    size: Int,
+    step: Int = 1,
+    partialWindows: Boolean = false,
+    crossinline transform: (IntArrayList) -> IntArrayList = { it },
+): Sequence<IntArrayList> = sequence {
+    size.requireGt(0, "size")
+    step.requireGt(0, "step")
+
+    var pos = 0
+    val intervalSize = this@windowed.size()
+    while (pos < intervalSize) {
+        val window = IntArrayList()
+        repeat(size) {
+            if (pos + it < intervalSize) {
+                window.add(this@windowed[pos + it])
+            }
         }
+        when {
+            window.size() == size -> yield(transform(window))
+            partialWindows -> yield(transform(window))
+        }
+        pos += step
+    }
 }
 
+inline fun IntInterval.chunked(
+    chunkSize: Int,
+    partialWindows: Boolean = true,
+    crossinline transform: (IntArrayList) -> IntArrayList = { it },
+): Sequence<IntArrayList> =
+    windowed(chunkSize, chunkSize, partialWindows, transform)
 
-fun intLongervalOf(start: Long, endInclusive: Long, step: Long = 1): LongInterval =
+inline fun IntInterval.sliding(
+    size: Int,
+    partialWindows: Boolean = true,
+    crossinline transform: (IntArrayList) -> IntArrayList = { it },
+): Sequence<IntArrayList> =
+    windowed(size, 1, partialWindows, transform)
+
+fun longIntervalOf(start: Long, endInclusive: Long, step: Long = 1): LongInterval =
     LongInterval.fromToBy(start, endInclusive, step)
 
 fun LongInterval.toLongArrayList(): LongArrayList = longArrayList(this.size()) { it.toLong() }
@@ -37,10 +67,42 @@ inline fun LongInterval.forEach(crossinline block: (Long) -> Unit) {
     this.each { block(it) }
 }
 
-fun LongInterval.chunked(chunkSize: Int): Sequence<LongArrayList> = sequence {
-    this@chunked
-        .chunk(chunkSize)
-        .forEach { chunked ->
-            yield(chunked.toLongArrayList())
+inline fun LongInterval.windowed(
+    size: Int,
+    step: Int = 1,
+    partialWindows: Boolean = false,
+    crossinline transform: (LongArrayList) -> LongArrayList = { it },
+): Sequence<LongArrayList> = sequence {
+    size.requireGt(0, "size")
+    step.requireGt(0, "step")
+
+    var pos = 0
+    val intervalSize = this@windowed.size()
+    while (pos < intervalSize) {
+        val window = LongArrayList()
+        repeat(size) {
+            if (pos + it < intervalSize) {
+                window.add(this@windowed[pos + it])
+            }
         }
+        when {
+            window.size() == size -> yield(transform(window))
+            partialWindows -> yield(transform(window))
+        }
+        pos += step
+    }
 }
+
+inline fun LongInterval.chunked(
+    chunkSize: Int,
+    partialWindows: Boolean = true,
+    crossinline transform: (LongArrayList) -> LongArrayList = { it },
+): Sequence<LongArrayList> =
+    windowed(chunkSize, chunkSize, partialWindows, transform)
+
+inline fun LongInterval.sliding(
+    size: Int,
+    partialWindows: Boolean = true,
+    crossinline transform: (LongArrayList) -> LongArrayList = { it },
+): Sequence<LongArrayList> =
+    windowed(size, 1, partialWindows, transform)
