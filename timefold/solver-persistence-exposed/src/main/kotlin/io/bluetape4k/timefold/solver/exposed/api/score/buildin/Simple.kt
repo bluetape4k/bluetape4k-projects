2423c5840f56c282f132035718677601cbe03062
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-10-19 20:48:22 +0900

    feat: Timefold SimpleScore 를 저장할 수 있는 Exposed 컬럼 타입 추가

--- timefold/solver-persistence-exposed/src/main/kotlin/io/bluetape4k/timefold/solver/exposed/api/score/buildin/Simple.kt
+++ timefold/solver-persistence-exposed/src/main/kotlin/io/bluetape4k/timefold/solver/exposed/api/score/buildin/Simple.kt
@@ -1,3 +1,33 @@
 package io.bluetape4k.timefold.solver.exposed.api.score.buildin
 
+import ai.timefold.solver.core.api.score.buildin.simple.SimpleScore
+import org.jetbrains.exposed.v1.core.Column
+import org.jetbrains.exposed.v1.core.ColumnTransformer
+import org.jetbrains.exposed.v1.core.ColumnWithTransform
+import org.jetbrains.exposed.v1.core.Table
+import org.jetbrains.exposed.v1.core.VarCharColumnType
+
 // SimpleScoreColumnType
+
+/**
+ * Timefold 의 [SimpleScore] 를 저장할 수 있는 Column 을 생성합니다.
+ */
+fun Table.simpleScore(
+    name: String,
+    length: Int,
+): Column<SimpleScore> = registerColumn(name, SimpleScoreColumnType(length))
+
+class SimpleScoreColumnType(length: Int):
+    ColumnWithTransform<String, SimpleScore>(VarCharColumnType(length), SimpleScoreTransformer())
+
+class SimpleScoreTransformer(): ColumnTransformer<String, SimpleScore> {
+    /**
+     * Entity Property 를 DB Column 수형으로 변환합니다.
+     */
+    override fun unwrap(value: SimpleScore): String = value.toString()
+
+    /**
+     * DB Column 값을 Entity Property 수형으로 변환합니다.
+     */
+    override fun wrap(value: String): SimpleScore = SimpleScore.parseScore(value)
+}
