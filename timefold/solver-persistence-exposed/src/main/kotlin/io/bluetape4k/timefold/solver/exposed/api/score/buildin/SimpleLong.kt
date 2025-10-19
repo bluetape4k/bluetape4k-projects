c6fd9f2c0981a0ce3010e34151a3311f6501e8af
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-10-19 21:49:55 +0900

    feat: Timefold SimpleLongScore 를 저장할 수 있는 Exposed 컬럼 타입 추가

---
+++ timefold/solver-persistence-exposed/src/main/kotlin/io/bluetape4k/timefold/solver/exposed/api/score/buildin/SimpleLong.kt
@@ -0,0 +1,30 @@
+package io.bluetape4k.timefold.solver.exposed.api.score.buildin
+
+import ai.timefold.solver.core.api.score.buildin.simplelong.SimpleLongScore
+import org.jetbrains.exposed.v1.core.Column
+import org.jetbrains.exposed.v1.core.ColumnTransformer
+import org.jetbrains.exposed.v1.core.ColumnWithTransform
+import org.jetbrains.exposed.v1.core.LongColumnType
+import org.jetbrains.exposed.v1.core.Table
+
+/**
+ * Timefold 의 [SimpleLongScore] 를 저장할 수 있는 Column 을 생성합니다.
+ */
+fun Table.simpleLongScore(
+    name: String,
+): Column<SimpleLongScore> = registerColumn(name, SimpleLongScoreColumnType())
+
+class SimpleLongScoreColumnType():
+    ColumnWithTransform<Long, SimpleLongScore>(LongColumnType(), SimpleLongScoreTransformer())
+
+class SimpleLongScoreTransformer(): ColumnTransformer<Long, SimpleLongScore> {
+    /**
+     * Entity Property 를 DB Column 수형으로 변환합니다.
+     */
+    override fun unwrap(value: SimpleLongScore): Long = value.score()
+
+    /**
+     * DB Column 값을 Entity Property 수형으로 변환합니다.
+     */
+    override fun wrap(value: Long): SimpleLongScore = SimpleLongScore.of(value)
+}
