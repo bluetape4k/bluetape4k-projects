419aa24fe3ba81ff0e06290a6769881b44b23dbd
debop <sunghyouk.bae@gmail.com>
2026-02-11 05:06:21 +0900

    refactor: KotlinDelegates 유틸리티 함수 개선 및 예외 처리 강화
    - `KotlinDelegates`:
      - `findPrimaryConstructor`: Java 생성자 조회 실패 시 예외를 던지는 대신 에러 로그를 남기고 `null`을 반환하도록 변경. 로그 메시지 오타 수정 (`findl` -> `find`).
      - `instantiateClass`: 인스턴스 생성 로직에 `try-catch`를 적용하여 예외 발생 시 `null`을 반환하도록 안전성 강화. 인자 검증 메시지 오타 수정 및 `HashMap` 생성 최적화.
      - 주요 함수들의 KDoc 주석 태그를 `@receiver`에서 `@param`으로 수정하여 명확성 개선.

--- bluetape4k/core/src/main/kotlin/io/bluetape4k/utils/KotlinDelegates.kt
+++ bluetape4k/core/src/main/kotlin/io/bluetape4k/utils/KotlinDelegates.kt
@@ -33,7 +33,7 @@ object KotlinDelegates: KLogging() {
 
     /**
      * 지정한 수형의 기본 생성자를 가져옵니다. 없으면 예외 [NoSuchElementException]를 발생시킵니다.
-     * @receiver Class<T>
+     * @param clazz 대상 클래스
      * @return Constructor<T>
      */
     fun <T: Any> primaryConstructor(clazz: Class<T>): Constructor<T> =
@@ -42,43 +42,50 @@ object KotlinDelegates: KLogging() {
 
     /**
      * 지정한 수형의 기본 생성자 정보를 찾습니다. 없으면 null 반환
-     * @receiver 수형 정보
-     * @return 생성자 정보
+     * @param clazz 대상 클래스
+     * @return 생성자 정보, 없으면 null
      */
     fun <T: Any> findPrimaryConstructor(clazz: Class<T>): Constructor<T>? {
         return try {
             val primaryCtor = clazz.kotlin.primaryConstructor ?: return null
-            primaryCtor.javaConstructor
-                ?: error("Fail to find Java constructor for Kotlin primary constructor: ${clazz.name}")
+            primaryCtor.javaConstructor.also {
+                if (it == null) {
+                    log.error { "Fail to find Java constructor for Kotlin primary constructor: ${clazz.name}" }
+                }
+            }
         } catch (e: UnsupportedOperationException) {
-            log.error(e) { "Fail to findl primary constructor of Kotlin class [${clazz.name}]" }
+            log.error(e) { "Fail to find primary constructor of Kotlin class [${clazz.name}]" }
             null
         }
     }
 
     /**
      * 생성자 정보를 이용하여 `T` 수형의 인스턴스를 생성합니다.
-     * @receiver Constructor<T>
-     * @param args Array<out Any?> 생성자의 인자들
-     * @return T? 생성된 인스턴스 또는 null
+     * @param constructor 생성자 정보
+     * @param args 생성자의 인자들
+     * @return 생성된 인스턴스 또는 null
      */
     fun <T: Any> instantiateClass(constructor: Constructor<T>, vararg args: Any?): T? {
+        return try {
+            val kotlinCtor = constructor.kotlinFunction ?: return constructor.newInstance(*args)
 
-        val kotlinCtor = constructor.kotlinFunction ?: return constructor.newInstance(*args)
+            val parameters = kotlinCtor.parameters
 
-        val parameters = kotlinCtor.parameters
-        val argParams = hashMapOf<KParameter, Any?>()
-
-        check(args.size <= parameters.size) {
-            "Number of provided argumenets should be less of equals than number of constructor parameters."
-        }
-
-        args.forEachIndexed { i, arg ->
-            val isOptional = parameters[i].isOptional && arg == null
-            if (!isOptional) {
-                argParams[parameters[i]] = arg
+            check(args.size <= parameters.size) {
+                "Number of provided arguments should be less than or equal to number of constructor parameters."
             }
+
+            val argParams = HashMap<KParameter, Any?>(parameters.size)
+            args.forEachIndexed { i, arg ->
+                val isOptional = parameters[i].isOptional && arg == null
+                if (!isOptional) {
+                    argParams[parameters[i]] = arg
+                }
+            }
+            kotlinCtor.callBy(argParams)
+        } catch (e: Exception) {
+            log.error(e) { "Fail to instantiate class [${constructor.declaringClass.name}]" }
+            null
         }
-        return kotlinCtor.callBy(argParams)
     }
 }
