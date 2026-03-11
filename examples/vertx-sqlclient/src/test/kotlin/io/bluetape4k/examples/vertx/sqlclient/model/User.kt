fa64ee3c2d3efab662e58007de4c8cf310c1403d
debop <sunghyouk.bae@gmail.com>
2026-03-11 18:07:16 +0900

    chore: vertx 관련 예제 제거 (사용하지 않는다)
    Signed-off-by: debop <sunghyouk.bae@gmail.com>

--- examples/vertx-sqlclient/src/test/kotlin/io/bluetape4k/examples/vertx/sqlclient/model/User.kt
+++
@@ -1,9 +0,0 @@
-package io.bluetape4k.examples.vertx.sqlclient.model
-
-import java.io.Serializable
-
-data class User(
-    val id: Long,
-    val firstName: String,
-    val lastName: String,
-): Serializable
--- examples/vertx-sqlclient/src/test/kotlin/io/bluetape4k/examples/vertx/sqlclient/package-info.java
+++
@@ -1,4 +0,0 @@
-@ModuleGen(name = "vertx-sqlclient", groupPackage = "io.bluetape4k.examples.vertx.sqlclient")
-package io.bluetape4k.examples.vertx.sqlclient;
-
-import io.vertx.codegen.annotations.ModuleGen;
