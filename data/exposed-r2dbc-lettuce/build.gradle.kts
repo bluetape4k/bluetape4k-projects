d3f0f5427dfb5b7e352b6fdf83aec30fca8a6fde
debop <sunghyouk.bae@gmail.com>
2026-03-17 20:44:32 +0900

    chore: exposed-r2dbc-lettuce 모듈 스캐폴딩 추가

---
+++ data/exposed-r2dbc-lettuce/build.gradle.kts
@@ -0,0 +1,38 @@
+configurations {
+    testImplementation.get().extendsFrom(compileOnly.get(), runtimeOnly.get())
+}
+
+dependencies {
+    api(project(":bluetape4k-lettuce"))
+    api(project(":bluetape4k-exposed-r2dbc"))
+
+    // Exposed R2DBC
+    api(Libs.exposed_core)
+    api(Libs.exposed_r2dbc)
+    compileOnly(Libs.exposed_java_time)
+    compileOnly(Libs.exposed_kotlin_datetime)
+
+    // Serializer (LettuceLoadedMap 코덱용)
+    compileOnly(Libs.fory_kotlin)
+    compileOnly(Libs.kryo5)
+
+    // Compressor
+    compileOnly(Libs.snappy_java)
+    compileOnly(Libs.lz4_java)
+    compileOnly(Libs.zstd_jni)
+
+    // Coroutines (R2DBC suspend 브리징)
+    implementation(project(":bluetape4k-coroutines"))
+    implementation(Libs.kotlinx_coroutines_core)
+    implementation(Libs.kotlinx_coroutines_reactive)
+
+    // R2DBC drivers (test)
+    testRuntimeOnly(Libs.r2dbc_h2)
+
+    testImplementation(project(":bluetape4k-junit5"))
+    testImplementation(project(":bluetape4k-testcontainers"))
+    testImplementation(project(":bluetape4k-exposed-r2dbc-tests"))
+    testImplementation(Libs.h2_v2)
+    testImplementation(Libs.kotlinx_coroutines_test)
+    testImplementation(project(":bluetape4k-idgenerators"))
+}
