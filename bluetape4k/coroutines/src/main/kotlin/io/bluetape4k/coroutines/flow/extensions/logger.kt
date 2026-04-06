8c9a966811dc2c92f9acf12acca691e6f46245d2
debop <sunghyouk.bae@gmail.com>
2026-04-06 16:20:16 +0900

    fix: AsyncFlow.log() 함수가 로그를 출력하지 않는 버그 수정
    Signed-off-by: debop <sunghyouk.bae@gmail.com>

--- bluetape4k/coroutines/src/main/kotlin/io/bluetape4k/coroutines/flow/extensions/logger.kt
+++ bluetape4k/coroutines/src/main/kotlin/io/bluetape4k/coroutines/flow/extensions/logger.kt
@@ -1,6 +1,8 @@
 package io.bluetape4k.coroutines.flow.extensions
 
 import io.bluetape4k.LibraryName
+import io.bluetape4k.coroutines.flow.AsyncFlow
+import io.bluetape4k.coroutines.flow.async
 import io.bluetape4k.logging.KotlinLogging
 import io.bluetape4k.logging.debug
 import kotlinx.coroutines.CancellationException
@@ -31,7 +33,52 @@ internal val logger by lazy { KotlinLogging.logger(LibraryName) }
  * @param log 출력에 사용할 SLF4J Logger입니다.
  */
 @Suppress("IMPLICIT_CAST_TO_ANY", "UNCHECKED_CAST")
-fun <F: Flow<T>, T> F.log(tag: Any, log: org.slf4j.Logger = logger): F {
+fun <T> Flow<T>.log(tag: Any, log: org.slf4j.Logger = logger): Flow<T> {
+    return this
+        .onStart {
+            log.debug { "[$tag] \uD83D\uDE80" }
+        }
+        .onEmpty {
+            log.debug { "[$tag] \uD83D\uDEAB" }
+        }
+        .onEach {
+            val item = when (it) {
+                is Flow<*> -> it.toList()
+                else       -> it
+            }
+            log.debug { "[$tag] ➡️emit $item" }
+        }
+        .onCompletion {
+            if (it == null) {
+                log.debug { "[$tag] ✅" }
+            } else {
+                when (it) {
+                    is CancellationException -> log.debug { "[$tag] \uD83D\uDEAB" }
+                    else                     -> log.debug(it) { "[$tag] 🔥" }
+                }
+            }
+        }
+}
+
+/**
+ * AsyncFlow 생명주기(start/each/empty/completion)를 디버그 로그로 기록합니다.
+ *
+ * ## 동작/계약
+ * - `onStart`, `onEach`, `onEmpty`, `onCompletion` 연산자를 연결한 새 Flow를 반환합니다.
+ * - 요소가 `Flow<*>` 타입이면 `toList()`로 풀어서 로그 문자열을 구성하므로 추가 수집/할당이 발생할 수 있습니다.
+ * - 예외 종료는 `🔥`, 취소는 `🚫`, 정상 완료는 `✅`로 구분해 기록합니다.
+ *
+ * ```kotlin
+ * val traced = flowOf(1, 2).log("sample")
+ * traced.collect()
+ * // 로그: [sample] 🚀, [sample] ➡️emit 1, [sample] ➡️emit 2, [sample] ✅
+ * ```
+ *
+ * @param tag 로그 메시지에 포함할 태그입니다.
+ * @param log 출력에 사용할 SLF4J Logger입니다.
+ */
+@Suppress("IMPLICIT_CAST_TO_ANY", "UNCHECKED_CAST")
+fun <T> AsyncFlow<T>.log(tag: Any, log: org.slf4j.Logger = logger): AsyncFlow<T> {
     return this
         .onStart {
             log.debug { "[$tag] \uD83D\uDE80" }
@@ -55,5 +102,6 @@ fun <F: Flow<T>, T> F.log(tag: Any, log: org.slf4j.Logger = logger): F {
                     else -> log.debug(it) { "[$tag] 🔥" }
                 }
             }
-        } as F
+        }
+        .async { it }
 }
