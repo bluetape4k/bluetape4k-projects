1c5c8bcec5e359c6976fe6406fc71fcef98ac94d
debop <sunghyouk.bae@gmail.com>
2026-04-23 21:29:50 +0900

    build(deps): Bump up kotlin-stdlib (2.3.20 -> 2.3.21)
    Signed-off-by: debop <sunghyouk.bae@gmail.com>

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -68,7 +68,7 @@ object Plugins {
 
 object Versions {
 
-    const val kotlin = "2.3.20"                 // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
+    const val kotlin = "2.3.21"                 // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
     const val kotlinx_coroutines = "1.10.2"     // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
     const val kotlinx_serialization = "1.11.0"  // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-serialization-json-jvm
     const val kotlinx_atomicfu = "0.32.1"      // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/atomicfu
